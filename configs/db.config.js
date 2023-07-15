581f0de51845e33d286466eae0a35a135b31bba3
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-07-15 13:09:01 +0530

    admin user

---
+++ configs/db.config.js
@@ -0,0 +1,4 @@
+module.exports ={
+    DB_NAME : "idea_app_demo",
+    DB_URL: "mongodb+srv://mayanktrad365:kcCneqQgwsLUxrEy@cluster0.ybqalak.mongodb.net/?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,9 +1,51 @@
 const express = require("express")
 const serverConfig = require("./configs/server.config.js")
-
+const dbConfig = require("./configs/db.config.js")
+const mongoose = require("mongoose")
+// const { init } = require("./models/user.model.js")
 const app = express()
+const userModel = require("./models/user.model.js")
 
 app.listen(serverConfig.PORT ,()=>{
 console.log(`server started on port ${serverConfig}`)
 })
 
+mongoose.connect(dbConfig.DB_URL).then(()=>{
+    console.log("connected")
+    init()
+})
+.catch((err)=>{
+    console.log(err, "error")
+})
+
+async function  init(){
+    // addmin user
+let admin = await userModel.findOne({
+    userId: "admin"
+})
+ 
+if(admin){
+    console.log("admin user alread")
+    return;
+}
+     admin = await userModel.create({
+        name: "Iron Man",
+        userId: "iron",
+        email: "tonyStark@gmail.com",
+        userType: "ADMIN",
+        password:"jarvis"
+    })
+console.log(admin)
+}
+
+// const db = mongoose.connection;
+
+// db.on("error", ()=>{
+//     console.log("error while connecting")
+// })
+
+// db.once("open", ()=>{
+//     console.log("Db is connected")
+// })
+// 
+// kcCneqQgwsLUxrEy
